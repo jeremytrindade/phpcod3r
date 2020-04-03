1bf7ea81a05ee1058ad1a009239f2395c4dca762
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 15:57:32 +0200

    28. aé 30 incluido

--- CURSO-PHP/index.php
+++ CURSO-PHP/index.php
@@ -25,6 +25,8 @@
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=desafio">Desafio</a></li>
                     </ul>
                 </div>
             </nav>
