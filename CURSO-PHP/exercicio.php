6b375e110412cd2b05e6ed6a61a23dfd7cb04641
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 11:12:45 +0200

    17. Navegação #01

--- CURSO-PHP/exercicio.php
+++ CURSO-PHP/exercicio.php
@@ -5,10 +5,14 @@
     <meta charset="UFT-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
     <title>Exercício</title>
 </head>
-
-<body>
+<nav class="navegacao">
+    <a href="#" class="verde">Sem formatação</a>
+    <a href="index.php" class="vermelho">Voltar</a>
+</nav>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
