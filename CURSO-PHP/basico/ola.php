d692522cdfcf828871e8a1bf2b34c6399463cf43
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 14:51:13 +0200

    25. Olá PHP

---
+++ CURSO-PHP/basico/ola.php
@@ -0,0 +1,13 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php
+echo 'Olá <br>';
+/* echo "\n"; Quebra de linha em php, mas não funciona no html*/
+echo 'Mundo!';
+?>
+<h2>Exemplo B</h2>
+<?= "Outra forma de me 'expressar'!" ?> <!-- Imprime diretamente no html-->
+
+<?php
+phpinfo();
