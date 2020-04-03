13e639d30849dc1d8d68b9bb4c8dc08bb9d7fc01
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 11:36:24 +0200

    18. navegação #03

--- CURSO-PHP/exercicio.php
+++ CURSO-PHP/exercicio.php
@@ -9,7 +9,7 @@
     <title>Exercício</title>
 </head>
 <nav class="navegacao">
-    <a href="#" class="verde">Sem formatação</a>
+    <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
     <a href="index.php" class="vermelho">Voltar</a>
 </nav>
 <body class="exercicio">
