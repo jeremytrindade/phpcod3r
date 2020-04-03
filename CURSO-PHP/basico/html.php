ca35787beef72bfeb83cee07acfc756ad76fa155
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 15:02:20 +0200

    26. Integração HTML

---
+++ CURSO-PHP/basico/html.php
@@ -0,0 +1,14 @@
+<div class="titulo">Integração HTML</div>
+<h1>
+<?php
+echo 'Olá';
+echo '<small>';
+echo ' Mundo!';
+echo '</small>';
+?>
+</h1>
+<!-- Esta linha só vai imprimir uma string -->
+<?= "<div> Outra forma de me 'expressar'!</div>" ?>
+
+<br>
+<div><button><?= "Legal" ?></button></div>
