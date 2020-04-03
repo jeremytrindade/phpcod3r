7ee5011980a0264459e6cedad4392c68875cc71f
Jérémy Trindade <jeremytrindade@gmail.com>
2020-04-03 19:49:38 +0200

    39. Desafio Precedência

--- CURSO-PHP/index.php
+++ CURSO-PHP/index.php
@@ -37,6 +37,7 @@
                         <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
                         <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
                         <li><a href="exercicio.php?dir=tipos&file=aritemeticas">Op. Aritméticas</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=desafio_precedencia">Desafio Precedência</a></li>
                     </ul>
                 </div>
             </nav>
